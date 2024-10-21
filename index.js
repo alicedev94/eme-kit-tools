ae64e6856299f093dd3bfa91623ae00a2cece9f7
Dilan Marcano <d.marcano@DAKA.LOCAL>
2024-10-21 15:56:26 -0400

    prepare to production

---
+++ index.js
@@ -0,0 +1,7 @@
+import './dist/base.css';
+import './dist/color.css';
+import './dist/custom.css';
+import './dist/normalize_v8.0.1.css';
+import './dist/responsive_desing.css';
+import './dist/theme.css';
+import './dist/utils.css';
